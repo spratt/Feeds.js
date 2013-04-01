1cf568734e5eada4512c473232469d19e8503f87
Simon Pratt <simon@pr4tt.com>
2013-04-01 01:05:57 -0400

    Implemented blog groups

--- Feeds.js
+++ Feeds.js
@@ -3,23 +3,23 @@
 google.load("feeds", "1");
 
 $(function() {
-	var feed_urls;
-	var feed_entries = [];
-	var container = $('#container');
+	var feed_groups = [];
+	var container = $('#content');
+	var toc = $('#toc');
 	
 	function parseFeed(url, callback) {
 		(new google.feeds.Feed(url)).load(callback);
 	}
 
-	function addFeed(feed) {
+	function addFeed(group,feed) {
 		var new_entries = feed.entries;
 		new_entries.forEach(function(entry) {
 			if(entry.publishedDate === '') {
 				entry.publishedDate = (new Date(0)).toString();
 			}
 		});
-		feed_entries = feed_entries.concat(new_entries);
-		feed_entries.sort(function(a,b) {
+		group.entries = group.entries.concat(new_entries);
+		group.entries.sort(function(a,b) {
 			var aDateStr = a.publishedDate;
 			var aDate = new Date(aDateStr);
 
@@ -32,30 +32,50 @@ $(function() {
 
 	function refreshFeeds() {
 		container.text('');
-		feed_entries.forEach(function(entry) {
-			var entry_block = $('<div>');
-			var entry_link = $('<a>');
-			entry_link.attr('href',entry.link);
-			var entry_header = $('<h2>');
-			entry_header.text(entry.title);
-			entry_link.append(entry_header);
-			entry_block.append(entry_link);
-			var entry_content = $('<div>');
-			entry_content.html(entry.content);
-			entry_block.append(entry_content);
-			entry_block.appendTo(container);
+		toc.text('');
+		feed_groups.forEach(function(group) {
+			var toc_entry = $('<li>');
+			var toc_link = $('<a>');
+			toc_link.attr('href','#'+group.name);
+			toc_link.text(group.name);
+			toc_link.appendTo(toc_entry);
+			toc_entry.appendTo(toc);
+			var group_header = $('<h2>');
+			var group_anchor = $('<a>');
+			group_anchor.attr('name',group.name);
+			group_anchor.text(group.name);
+			group_anchor.appendTo(group_header);
+			group_header.appendTo(container);
+			group.entries.forEach(function(entry) {
+				var entry_block = $('<div>');
+				var entry_link = $('<a>');
+				entry_link.attr('href',entry.link);
+				var entry_header = $('<h3>');
+				entry_header.text(entry.title);
+				entry_link.append(entry_header);
+				entry_block.append(entry_link);
+				var entry_content = $('<div>');
+				entry_content.html(entry.content);
+				entry_block.append(entry_content);
+				entry_block.appendTo(container);
+			});
 		});
 	};
 
 	Feeds = function Feeds(feeds) {
-		feed_urls = feeds;
+		feed_groups = feeds;
+		feed_groups.forEach(function(group) {
+			group.entries = [];
+		});
 	};
 	
 	google.setOnLoadCallback(function() {
-		feed_urls.forEach(function(url) {
-			parseFeed(url,function(result) {
-				if(result.error) return;
-				addFeed(result.feed);
+		feed_groups.forEach(function(group) {
+			group.feeds.forEach(function(url) {
+				parseFeed(url,function(result) {
+					if(result.error) return;
+					addFeed(group,result.feed);
+				});
 			});
 		});
 	});
