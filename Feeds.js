3493ae0b561ae8936dcb266d88afc16e05e40f80
Simon Pratt <simon@pr4tt.com>
2013-04-04 17:42:09 -0400

    Cleaned up DOM code

--- Feeds.js
+++ Feeds.js
@@ -15,7 +15,6 @@ $(function() {
 	}
 
 	function addFeed(group,feed) {
-		console.dir(feed);
 		var new_entries = feed.entries;
 		new_entries.forEach(function(entry) {
 			if(entry.publishedDate === '') {
@@ -35,48 +34,60 @@ $(function() {
 		refreshFeeds();
 	};
 
+	function buildTOC(group) {
+		var toc_entry = $('<li>');
+		var toc_link = $('<a>');
+		toc_link.attr('href','#'+group.name);
+		toc_link.text(group.name);
+		toc_link.appendTo(toc_entry);
+		toc_entry.appendTo(toc);
+	}
+
+	function buildFeed(group) {
+		var group_header = $('<h2>');
+		var group_anchor = $('<a>');
+		group_anchor.attr('name',group.name);
+		group_anchor.text(group.name);
+		group_anchor.appendTo(group_header);
+		group_header.appendTo(container);
+		group.entries.forEach(function(entry) {
+			buildEntry(entry,group);
+		});
+	}
+
+	function buildEntry(entry,group) {
+		var entry_block = $('<div>');
+		entry_block.addClass('spaced');
+		var entry_link = $('<a>');
+		entry_link.attr('href',entry.link);
+		var entry_header = $('<h3>');
+		entry_header.addClass('inline');
+		entry_header.text(entry.title);
+		entry_link.append(entry_header);
+		entry_block.append(entry_link);
+		if(group.feeds.length > 1) {
+			var entry_source = $('<h4>');
+			entry_source.addClass('inline');
+			entry_source.text('(' + entry.feedName + ')');
+			entry_block.append(entry_source);
+		}
+		if(entry.author !== '' && entry.author !== 'editors') {
+			var entry_author = $('<span>');
+			entry_author.text('By: ' + entry.author);
+			entry_block.append(entry_author);
+		}
+		var entry_content = $('<div>');
+		entry_content.html(entry.content);
+		entry_block.append(entry_content);
+		entry_block.appendTo(container);
+	}
+
 	function refreshFeeds() {
 		container.text('');
 		toc.text('');
 		feed_groups.forEach(function(group) {
-			var toc_entry = $('<li>');
-			var toc_link = $('<a>');
-			toc_link.attr('href','#'+group.name);
-			toc_link.text(group.name);
-			toc_link.appendTo(toc_entry);
-			toc_entry.appendTo(toc);
-			var group_header = $('<h2>');
-			var group_anchor = $('<a>');
-			group_anchor.attr('name',group.name);
-			group_anchor.text(group.name);
-			group_anchor.appendTo(group_header);
-			group_header.appendTo(container);
-			group.entries.forEach(function(entry) {
-				var entry_block = $('<div>');
-				entry_block.addClass('spaced');
-				var entry_link = $('<a>');
-				entry_link.attr('href',entry.link);
-				var entry_header = $('<h3>');
-				entry_header.addClass('inline');
-				entry_header.text(entry.title);
-				entry_link.append(entry_header);
-				entry_block.append(entry_link);
-				if(group.feeds.length > 1) {
-					var entry_source = $('<h4>');
-					entry_source.addClass('inline');
-					entry_source.text('(' + entry.feedName + ')');
-					entry_block.append(entry_source);
-				}
-				if(entry.author !== '' && entry.author !== 'editors') {
-					var entry_author = $('<span>');
-					entry_author.text('By: ' + entry.author);
-					entry_block.append(entry_author);
-				}
-				var entry_content = $('<div>');
-				entry_content.html(entry.content);
-				entry_block.append(entry_content);
-				entry_block.appendTo(container);
-			});
+			buildTOC(group);
+			buildFeed(group);
 		});
 	};
 
